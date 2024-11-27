2a59724193396411adae786c8668aafd26dcac3c
RoRoBook <ronaldoroig@outlook.com>
2024-11-27 16:29:29 -0300

    Implementada a validação de movimento de instâncias de peças em relação a sua posição no tabuleiro 8x8. Iniciada a implementação de validação  de movimento na classe peão, necessário rever o projeto para adequar um atributo que registre a posição da instância filha de peça em relação ao tabuleiro.

--- poo/trab_2/Marbles/Peao.java
+++ poo/trab_2/Marbles/Peao.java
@@ -1,9 +1,11 @@
 package Marbles;
 
 public class Peao extends Pecas{
+    private boolean primeiroMovimento;
 
     public Peao (Cor cor) {
         super(cor);
+        this.primeiroMovimento = true;
     }
     
     @Override
@@ -17,6 +19,28 @@ public class Peao extends Pecas{
         }
 
         return corPeca;
+    }
+
+    public boolean movimentoValido(Object[] pecaEMovimento) {
+        
+        boolean valido = false;
+
+        int[] parOrdenado = (int[]) pecaEMovimento[1];
+
+        if (super.movimentoValido(parOrdenado)) /* Buscar tratar exceções para movimentos inválidos enquanto instância de peça
+        para retornar ao usuário como prosseguir */{
+
+            if(this.primeiroMovimento) {
+                /* A partir daqui é necessário ter o registro da posição atual de cada peça, atualmente no código não existe
+                 * nenhum atributo responsável por isso, a única classe que sabe a localização de determinada peça é a classe Tabuleiro.
+                 * É necessário implementar o registro das posições das peças antes de seguir a validação do movimento das instâncias de
+                 * classe filhas.
+                 */
+            }
+
+        }
+
+        return valido;
     }
 
 }
--- poo/trab_2/Marbles/Pecas.java
+++ poo/trab_2/Marbles/Pecas.java
@@ -1,8 +1,9 @@
 package Marbles;
 
 public abstract class Pecas {
-
 private Cor cor;
+private int[] parOrdenado; /* Atributo que guardará o par ordenado (x e depois y, portanto parOrdenado[0] = x e ...[1] = y)
+necessário implementar método set que extraia da instância de tabuleiro a posição da instância de peças nele. */
 
 public Pecas(Cor cor) {
     this.cor = cor;
@@ -17,7 +18,12 @@ public void setCor(Cor cor) {
 }
 
 public boolean movimentoValido(int[] parOrdenado) {
+
     boolean valido = false;
+
+    if (parOrdenado[0] < 8 && parOrdenado[1] < 8) {
+        valido = true;
+    }
 
     return valido;
 }
