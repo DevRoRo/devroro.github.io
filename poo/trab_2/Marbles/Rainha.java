5a5f0ccb616ce6cd7a1ff0bd4ad233fbfc6adc78
RoRoBook <ronaldoroig@outlook.com>
2024-12-06 18:05:52 -0300

    corrigido bug na movimentação da rainha.

--- poo/trab_2/Marbles/Rainha.java
+++ poo/trab_2/Marbles/Rainha.java
@@ -69,10 +69,21 @@ public class Rainha extends Pecas {
     public boolean movimentoValido(Tabuleiro jogo, Posicao posicao) {
 
         if (super.movimentoValido(jogo, posicao)) {
-            if (new Bispo(this.getCor()).movimentoValido(jogo, posicao)) {
+
+            Pecas bispo = new Bispo(this.getCor());
+            bispo.setPosicaoAtual(this.getPosicaoAtual());
+
+            Pecas torre = new Torre(this.getCor());
+            torre.setPosicaoAtual(this.getPosicaoAtual());
+
+            if (bispo.movimentoValido(jogo, posicao)) {
                 return true;
+<<<<<<< HEAD
             } else if (new Torre(this.getCor()).movimentoValido(jogo, posicao)) {
 >>>>>>> 4e77ca7 (Tabuleiro funcional, todas as peças devidamente geradas, movimento funcional e restringidos para sobreposição de mesma cor, manter no mesmo lugar, sair para além do tabuleiro, respeito as regras de cada peça, sistema de jogadores com time de peças específicos, método alternativo de encerrar jogo com base na presença da peça rainha e declaração de vencedor.)
+=======
+            } else if (torre.movimentoValido(jogo, posicao)) {
+>>>>>>> f0aaecf (corrigido bug na movimentação da rainha.)
                 return true;
             } else {
                 return false;
