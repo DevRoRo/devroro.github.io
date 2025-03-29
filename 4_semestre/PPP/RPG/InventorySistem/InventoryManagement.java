b3b23d474ac5b5872c1060b485a3af6d2960457f
RoRoBook <ronaldoroig@outlook.com>
2025-03-29 15:26:21 -0300

    Movido o RPG pra fora da pasta TADS. reorganizados os arquivos do 4_semestre

--- 4_semestre/PPP/RPG/InventorySistem/InventoryManagement.java
+++
@@ -1,9 +0,0 @@
-package InventorySistem;
-
-public interface InventoryManagement {
-
-    public void equipWeapontoCombatHand();
-
-    public void UnequipWeaponfromCombatHand();
-
-}
--- 4_semestre/estrutura_dados/main.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main() {
-int idade, idade2;
-scanf("%d", &idade);
-printf("\n Informe outra idade:");
-}
