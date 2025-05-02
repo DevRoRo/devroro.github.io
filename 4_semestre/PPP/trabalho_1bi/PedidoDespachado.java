aa212717df533f4bfa4f23459d447ec774ee121b
Ronaldo <ronaldoroig@outlook.com>
2025-05-01 21:24:10 -0300

    Feito exercício 4 trabalhão 1 bimestre.

--- 4_semestre/PPP/trabalho_1bi/PedidoDespachado.java
+++
@@ -1,9 +0,0 @@
-public class PedidoDespachado implements State {
-
-    @Override
-    public State status() {
-        System.out.println("Seu pedido foi despachado. Acompanhe o trajeto até a rota de entrega.");
-        return new PedidoEntegue();
-    }
-    
-}
